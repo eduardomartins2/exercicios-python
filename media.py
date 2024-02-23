d86c30c9f64d95e888125dbde75693f5900ee612
Eduardo Martins <eduardo.amaral382@gmail.com>
2024-02-22 21:42:54 -0300

    Create media.py

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
 n1 = float(input("insira a primeira nota "))
+
 n2 = float(input("insira a segunda nota "))
+
 n3 = float(input("insira a terceira nota "))
+
 n4 = float(input("insira a quarta nota "))
-media = (n1 + n2 + n3 + n4)/4
+
+media = (n1 + n2 + n3 + n4) / 4
+
 print(f"Voce teve media de {media} em suas notas")

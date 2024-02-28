15c5ffd9d85a8c13f61dd1ab4fda019c72957922
Eduardo Martins <eduardo.amaral382@gmail.com>
2024-02-28 20:00:20 -0300

    Create m-for-cm.py

--- m-for-cm.py
+++ m-for-cm.py
@@ -1,3 +1,5 @@
-m = int (input("Quantos metros? "))
-cm = m * 100;
-print(f"centimetros {cm}cm
+m = int (input("Quantos metros quer converter? "))
+
+cm = m * 100
+
+print(f"{m} metros são {cm} centimetros")
